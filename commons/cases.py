6848ee23845da4e0cbc440a90df2844647e0106a
LL <ou.lee@channelwill.com>
2025-06-17 16:32:28 +0800

    添加变量缓存和接口断言入口

--- commons/cases.py
+++ commons/cases.py
@@ -1,14 +1,14 @@
 import logging
-
 import allure
-
 from commons.all_requests import request_client
+from commons.assert_cases import assert_cases
 from commons.module import verfiy_module
+from commons.save_extract import save_var
 
 logger = logging.getLogger(__name__)
 
 
-def excute_cases(info):
+def excute_cases(info: dict) -> None:
     """
     执行测试用例方法
     :param info: 用例数据
@@ -20,12 +20,15 @@ def excute_cases(info):
     logger.info(f"执行用例：{info.get('module')}==>{info.get('title')}")
     info = verfiy_module(info)
     try:
-        resp = request_client.send_request(**info['request'])
+        resp = request_client.send_request(**info.get('request'))
+        extract = info.get('extract')
+        validate = info.get('validate')
+        if extract:
+            logger.info(extract)
+            save_var(resp,extract)
 
-        if info['extract']:
-            pass
-
-        if info['validate']:
-            pass
+        if validate:
+            assert_cases(resp, validate)
     except Exception as e:
         logger.error(f'用例执行失败：{e}')
+        raise e
